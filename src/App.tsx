3162b6d71bcb63bf8a1827331be7b1c9e2f5496e
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-09-08 15:39:26 -0700

    BROKEN: Pass names as prop, not rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,50 @@
 import './App.css';
 
 import React, { useState } from 'react';
-import { render } from 'react-dom';
 
 // import logo from './logo.svg';
 
 // const [monList, setMonList] = useState([]);
 
 let response = await fetch('https://pokeapi.co/api/v2/generation/1');
-const generationData = response.json();
+const gen1Data = response.json();
 // generationData.then((data) => console.log(data.pokemon_species));
 
-const MonList = () => {
-  const nameList: String[] = [];
-  generationData.then((data) => {
-    nameList.push(...data.pokemon_species);
-  });
-  console.log(nameList);
-  //vv NEEDS WORK vv
-  const renderNames = nameList.map((mon) => <li key={mon}>{mon}M=</li>);
+const monList: { name: String; url: String }[] = [];
+const nameList: String[] = [];
 
+//Gather PKMN species from response data
+gen1Data.then((data) => {
+  monList.push(...data.pokemon_species);
+});
+console.log('monList:', monList);
+
+//Gather PKMN names to be rendered in <MonList />
+// vv NEEDS WORK vv loop is currently running 0 times >:|
+for (const mon of monList) {
+  console.log('pushing name: ' + mon.name);
+  nameList.push(mon.name);
+}
+
+const MonList = (props: { names: String[] }) => {
+  const [monNames, setMonNames] = useState<String[]>(props.names);
+  console.log('names:', monNames);
   return (
     <>
       <h2>Mon List</h2>
-      <div>{renderNames}</div>
+      <ul>
+        {monNames?.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
     </>
   );
 };
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <MonList />
+      <MonList names={nameList} />
     </div>
   );
 }
