00702594b40a504e15dc7194b7c8e88826804658
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-09-07 21:07:09 -0700

    [BROKEN] Fetched names from API, render WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,39 @@
 import './App.css';
 
 import React, { useState } from 'react';
+import { render } from 'react-dom';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+// const [monList, setMonList] = useState([]);
+
+let response = await fetch('https://pokeapi.co/api/v2/generation/1');
+const generationData = response.json();
+// generationData.then((data) => console.log(data.pokemon_species));
+
+const MonList = () => {
+  const nameList: String[] = [];
+  generationData.then((data) => {
+    nameList.push(...data.pokemon_species);
+  });
+  console.log(nameList);
+  //vv NEEDS WORK vv
+  const renderNames = nameList.map((mon) => <li key={mon}>{mon}M=</li>);
+
+  return (
+    <>
+      <h2>Mon List</h2>
+      <div>{renderNames}</div>
+    </>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
+      <MonList />
     </div>
   );
 }
