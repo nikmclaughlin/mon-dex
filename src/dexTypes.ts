ed067889e36a9de7d070272f56a0f61a86043bee
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-09-30 00:15:40 -0700

    Use proper name for NamedAPIResource type

--- src/dexTypes.ts
+++ src/dexTypes.ts
@@ -1,20 +1,20 @@
-export type ApiObject = {
+export type NamedAPIResource = {
   name: String;
   url: String;
 };
 
 export type PokemonEntry = {
   entry_number: Number;
-  pokemon_species: ApiObject;
+  pokemon_species: NamedAPIResource;
 };
 
 export type DexData = {
-  descriptions: { description: String; language: ApiObject }[];
+  descriptions: { description: String; language: NamedAPIResource }[];
   id: Number;
   is_main_series: boolean;
   name: String;
-  names: { language: ApiObject; name: String }[];
+  names: { language: NamedAPIResource; name: String }[];
   pokemon_entries: PokemonEntry[];
-  region: ApiObject;
-  version_groups: ApiObject[];
+  region: NamedAPIResource;
+  version_groups: NamedAPIResource[];
 };
