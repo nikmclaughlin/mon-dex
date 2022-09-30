31979051de25b4849478fa4f42ba494b9ecfb772
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-09-29 23:56:00 -0700

    Add sprites to Dex entries

--- src/components/MonList.tsx
+++ src/components/MonList.tsx
@@ -4,6 +4,24 @@ import { DexData, PokemonEntry } from '../dexTypes';
 
 const entriesList: PokemonEntry[] = [];
 
+/*/??TODO: Define individual entry as its own component
+function MonEntry(entryData: PokemonEntry) {
+  // const spriteURL: string = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${entryData?.entry_number}.png`;
+
+  // console.log('spriteURL: ', spriteURL);
+
+  return (
+    <>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png"
+        alt={'image of ' + entryData?.pokemon_species?.name}
+      />
+      <p>{entryData?.pokemon_species?.name}</p>
+    </>
+  );
+}
+/*/
+
 function MonList() {
   const [dexEntries, setDexEntries] = useState(entriesList);
 
@@ -32,7 +50,13 @@ function MonList() {
       <h2>Mon List</h2>
       <ul>
         {dexEntries?.map((entry) => (
-          <li key={entry?.entry_number}>{entry.pokemon_species.name}</li>
+          <li key={entry?.entry_number}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${entry?.entry_number}.png`}
+              alt={'image of ' + entry?.pokemon_species?.name}
+            />
+            <p>{entry?.pokemon_species?.name}</p>
+          </li>
         ))}
       </ul>
     </>
